c06c7a48228d7299a1711070e210f60cf9131d7d
aakalaydzhyan <aakalaydzhyan@avito.ru>
2022-05-08 22:43:46 +0300

    Add bootstrap and design shop's main page

--- src/main/java/ru/arinageek/ButcherShop/client/ClientController.java
+++ src/main/java/ru/arinageek/ButcherShop/client/ClientController.java
@@ -40,7 +40,7 @@ public class ClientController {
     @PostMapping(path = "/new")
     public String addUser(@ModelAttribute("user") Client client) {
         clientService.addUser(client);
-        return "redirect:/users";
+        return "redirect:/shop";
     }
 
     @PostMapping(path = "/delete/{id}")
@@ -61,7 +61,7 @@ public class ClientController {
         if (client != null) {
             sessionService.logInWithUserId(client.getId());
         }
-        return "redirect:/users";
+        return "redirect:/shop";
     }
 
 }
