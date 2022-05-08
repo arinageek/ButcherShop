885a432f377c3e6e40553d69f0404a1d13e75ad6
aakalaydzhyan <aakalaydzhyan@avito.ru>
2022-05-08 11:59:20 +0300

    Initial commit

---
+++ src/main/java/ru/arinageek/ButcherShop/ButcherShopApplication.java
@@ -0,0 +1,13 @@
+package ru.arinageek.ButcherShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcherShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButcherShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/arinageek/ButcherShop/ButcherShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arinageek.ButcherShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButcherShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
